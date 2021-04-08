715be9e5c3fcab3b063fba5894cbba1ac9700c6b
Jacek Milewski <jacek_milewski@hotmail.com>
2021-04-09 00:59:37 +0200

    Add printArray(), simple fillArray() and constructor of class
    IncreasingArray

--- main.cpp
+++ main.cpp
@@ -10,6 +10,34 @@ On each move, you may increase the value of any element by one. What is the mini
 
 class IncreasingArray
 {
+public:
+    IncreasingArray (int n)
+    {
+        vectorAsArray.resize(n);
+    }
+
+    void printArray() const
+    {
+        std::cout << "[ ";
+        for (std::size_t i = 0 ; i < vectorAsArray.size() ; i++ )
+        {
+            std::cout << vectorAsArray[i] << ", ";
+            //i++;
+
+        }
+        std::cout << "]\n";
+    }
+
+    void fillArray()
+    {
+        vectorAsArray.at(0) = 3;
+        vectorAsArray.at(1) = 2;
+        vectorAsArray.at(2) = 5;
+        vectorAsArray.at(3) = 1;
+        vectorAsArray.at(4) = 7;
+    }
+
+private:
     std::vector<int> vectorAsArray;
 
 };
@@ -20,6 +48,8 @@ class IncreasingArray
 
 int main()
 {
-
+    IncreasingArray array = IncreasingArray(5);
+    array.fillArray();
+    array.printArray();
     return 0;
 }
