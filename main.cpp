72b58850610680f9902aebdae2d042b3bfb191ba
Jacek Milewski <jacek_milewski@hotmail.com>
2021-04-09 01:39:32 +0200

    Add example to the exercise description

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@
 /*You are given an array of n integers. You want to modify the array so that it is increasing,
 i.e., every element is at least as large as the previous element.
 
-On each move, you may increase the value of any element by one. What is the minimum number of moves required?*/
+On each move, you may increase the value of any element by one. What is the minimum number of moves required?
+
+Example:
+
+Input:
+5
+3 2 5 1 7
+
+Output:
+5
+*/
 
 
 class IncreasingArray
