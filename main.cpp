d68691243075c0efb4f8550f980abbf9aa2b31b7
Jacek Milewski <jacek_milewski@hotmail.com>
2021-04-09 00:23:27 +0200

    Just started

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+
+/*You are given an array of n integers. You want to modify the array so that it is increasing,
+i.e., every element is at least as large as the previous element.
+
+On each move, you may increase the value of any element by one. What is the minimum number of moves required?*/
+
+
+class IncreasingArray
+{
+    std::vector<int> vectorAsArray;
+
+};
+
+
+
+
+
+int main()
+{
+
+    return 0;
+}
